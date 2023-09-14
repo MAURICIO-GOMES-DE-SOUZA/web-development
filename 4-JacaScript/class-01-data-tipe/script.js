9a9d0a923c3377f5829e6d1c0242a989b5aa6503
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2023-09-14 20:33:30 -0300

    add class prymite

---
+++ 4-JacaScript/class-01-data-tipe/script.js
@@ -0,0 +1,30 @@
+console.log("Hello Tijubina");
+// primiteves (string, Number, boolean...)
+
+let name = "Maurício";
+let age=23;
+let weight= 78.5;
+
+console.log(typeof student);
+console.log(typeof 23);
+console.log(age);
+console.log(typeof weight);
+
+let number1 = 2;
+let number2 = 5;
+console.log( number1 + number2);
+
+let isStudentInCvt = false;
+
+console.log(typeof isStudentInCvt);
+console.log(10 == 10);
+console.log(10 === "10");
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 != 20);
+
+let yourName = prompt("Digite seu nome: ");
+console.log(yourName);
+
+let Soma = prompt("Vamos somar!: ");
+console.log(yourName);
