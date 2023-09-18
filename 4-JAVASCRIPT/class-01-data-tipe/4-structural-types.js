18229909753a0fcb4b5ae22069f4879446024e4a
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2023-09-18 20:58:38 -0300

    update name variables

--- 4-JAVASCRIPT/class-01-data-tipe/4-structural-types.js
+++ 4-JAVASCRIPT/class-01-data-tipe/4-structural-types.js
@@ -5,22 +5,22 @@ let age = 22; //idade
 let profission = "Web Development";
 
 let student1 = { name: "Murício" }; //um objeto javascript é uma declaração de chave valor
-console.log(student.name);
+console.log(student1.name);
 
 console.log(nameStudent);
 let student2 = {
-  name: Mauricio,
+  name: "Mauricio",
   age: 22,
   profission: "Web Developmet",
   action: () => "estudou", //função, outro objeto estruturado.
 };
 
-console.log(student);
-console.log(typeof student);
-console.log(student.name);
-console.log(student.age);
-console.log(student["profission"]); //tbm utilizado em pyton
-console.log(student.action);
+console.log(student2);
+console.log(typeof student2);
+console.log(student2.name);
+console.log(student2.age);
+console.log(student2["profission"]); //tbm utilizado em pyton
+console.log(student2.action);
 
 //Array (Vector)
 //let pokedex = ["Charmander", 6,"Bulbasour", 5, "Squirtle", 4]; //voce pode usar outros tipos de dados. nm etc
