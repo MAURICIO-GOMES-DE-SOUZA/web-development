5c586c06459cf5a77ca0b09cece65a06cc13ca5c
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2023-09-26 18:36:16 -0300

    update project folders

---
+++ 4-JAVASCRIPT/Class-04-data-manipulation/string.js
@@ -0,0 +1,33 @@
+let myName = "Mauricio"
+
+console.log(myName.__proto__);
+console.log(myName.length);
+console.log(myName[1]);
+console.log(myName.tolocleUpperCase());
+console.log(myName.tolocleLowerrCase());
+console.log(myName.toUpperCase());
+
+console.log(myName);//String é immutable: imutável
+
+
+
+
+//let phrase = Im a web developer
+
+
+console.log(phrase.split(" ")[2] = "WEB");  //splt:separado
+console.log(phrase.split("").reverse().join("")) //sonre reescrever a string de forma invertida.
+
+
+//console.log(prase.replaceALL("web," "WEB"));
+console.log(prase);
+console.log(phrase.index0f("w"));
+console.log(phrase.trim());//remover espaço entes e depois
+
+console.log(phrase.substring(5));
+console.log(phrase.includes(web));
+console.log("web" == "Web"); // case-seneitive
+console.log(phrase.tolocaleLowerCase().icludes("web")?
+"Teste Verdadeiro" : "Teste Falso");
+
+console.log(phrase.includes);
