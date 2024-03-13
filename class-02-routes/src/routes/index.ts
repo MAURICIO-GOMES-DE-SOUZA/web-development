24310094e65e6576bc9878931e63131187413037
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2024-03-13 20:46:31 -0300

    add route params examples

--- class-02-routes/src/routes/index.ts
+++ class-02-routes/src/routes/index.ts
@@ -13,3 +13,24 @@ router.get("/pokemon", (_req, res) => {
         {id: 6, name: "Charizard"},
     ])
 })
+
+
+// orute params (obrigatório)
+router.get("/user/:id", (req, res) =>{
+    const { id } = req.params
+res.send({ id })
+})
+//query params (optional)
+
+router.get("/user", (req, res) => {
+    const {id, name, age }= req.query
+res.send({id, name, age})
+
+})
+//body params
+
+router.post("/user", (req, res) => {
+    const{id, name,age} = req.body
+   console.log({id, name,age})
+   res.send({status: "User create!"})
+})
--- class-02-routes/src/server.ts
+++ class-02-routes/src/server.ts
@@ -5,8 +5,9 @@ const app = express()
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`Server is running on PORT ${port}...`);
+    console.log(`Server is running on PORT ${port}... `);
     
 })
-
+//middleware
+app.use(express.json())
 app.use(router)
